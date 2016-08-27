82324d6675fc3231d5e2de9df6cf2f0aa5de08ac
pengfei.yu <scpengfei@gmail.com>
2016-08-27 16:06:10 +0800

    formatting code

--- src/main/java/dubbo/provider/service/impl/TestServiceImpl.java
+++ src/main/java/dubbo/provider/service/impl/TestServiceImpl.java
@@ -7,7 +7,9 @@ public class TestServiceImpl implements TestService {
      * 服务提供方实现接口：(对服务消费方隐藏实现)
      */
     public String sayHello(String name) {
-        System.out.println("调用provider服务");
-        return "hello"+name;
+        System.out.println("提供端：调用本端的sayHello服务");
+        System.out.println("提供端：sayHello服务：hello:" + name);
+        System.out.println("提供端：sayHello服务结束");
+        return "";
     }
 }
